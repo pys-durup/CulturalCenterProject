8d63d0cc7c619af8b3ca71544e8ff90e22b61c1a
durup <enenfnq@gmail.com>
2020-11-15 22:04:06 +0900

    project 기본세팅
    메인메서드 회원로그인 메뉴

--- CulturalCenter/src/com/project/center/employee/Employee.java
+++ CulturalCenter/src/com/project/center/employee/Employee.java
@@ -1,5 +1,6 @@
 package com.project.center.employee;
 
 public class Employee {
+	
 
 }
--- CulturalCenter/src/com/project/center/main/CulturalCenter.java
+++ CulturalCenter/src/com/project/center/main/CulturalCenter.java
@@ -7,59 +7,96 @@ public class CulturalCenter {
 		
 		// 유저, 직원, 관리자
 		
-//		while(true)	{
-//			int Num;
-//			
-//			// 메인화면 1. 로그인 2.회원가입	3.종료
-//			showMain();
-//			
-//			// 사용자가 입력한 숫자를 받아온다
-//			Num = selectNum(); 
-//			
-//			if(Num == 1) { // 로그인
-//				System.out.println("로그인 선택");
-//				// 로그인처리 메서드
-//				// public void isLogin()? 로그인처리?
-//				// public void createAccount(){} 로그인 성공하면?
-//				// 로그인 완료후 프로그램 진행
-//				break;
-//				
-//			} else if(Num == 2) { // 회원가입
-//				System.out.println("회원가입 선택");
-//				// 회원가입처리 메서드
-//				// 회원 가입 완료후 다시 메인화면으로
-//			} else if(Num == 3) {
-//				// 아이디/비밀번호 찾기 메서드
-//				
-//				
-//				
-//			} else if(Num == 4) { // 종료
-//				// 프로그램 종료
-//				System.out.println("프로그램을 종료합니다");
-//				System.exit(0); 
-//			} else {
-//				// 다른 숫자 입력시
-//				pause();
-//			}
-//			
-//		}
-		
-		int userType = 1; // 1 = 회원 2 = 직원 3 = 관리자
-		System.out.println("프로그램 진행 . . . .");
-		while (true) {
-			int Num; // 유저가 선택한 번호
+		while(true)	{
+			int userType = 1; // 1 = 회원 2 = 직원 3 = 관리자
+			int Num;
 			
-			// 1. 회원일때 2. 직원일때 3. 관리자 일때
-			if(userType == 1) {
-				// 회원에게 보여질 메뉴 출력
-				showUserMain();
-				Num = selectNum();
-			} else if(userType == 2) {
-				// 직원에게 보여질 메뉴 출력
-			} else if(userType == 3) {
-				// 관리자이게 보여질 메뉴 출력
+			// 메인화면 1. 로그인 2.회원가입	3.종료
+			showMain();
+			
+			// 사용자가 입력한 숫자를 받아온다
+			Num = selectNum(); 
+			
+			if(Num == 1) { // 로그인
+				System.out.println("로그인 선택");
+				// 로그인처리 메서드
+				// public void isLogin()? 로그인처리?
+				// public void createAccount(){} 로그인 성공하면?
+				// 로그인 완료후 프로그램 진행
+				
+				
+			} else if(Num == 2) { // 회원가입
+				System.out.println("회원가입 선택");
+				// 회원가입처리 메서드
+				// 회원 가입 완료후 다시 메인화면으로
+			} else if(Num == 3) {
+				// 아이디/비밀번호 찾기 메서드
+				
+				
+				
+			} else if(Num == 4) { // 종료
+				// 프로그램 종료
+				System.out.println("프로그램을 종료합니다");
+				System.exit(0); 
+			} else {
+				// 다른 숫자 입력시
+				pause();
+			}
+			
+			System.out.println("프로그램 진행 . . . .");
+			
+			
+			while (true) {
+				
+				
+				// 1. 회원일때 2. 직원일때 3. 관리자 일때
+				if(userType == 1) {
+					// 회원에게 보여질 메뉴 출력
+					showUserMain();
+					Num = selectNum();
+					
+					if(Num == 1) {
+						System.out.println("회원정보 조회");
+						
+					} else if (Num == 2) {
+						System.out.println("프로그램 신청");
+						
+					} else if (Num == 3) {
+						System.out.println("신청한 프로그램");
+						
+					} else if (Num == 4) {
+						System.out.println("진행중인 프로그램");
+						
+					} else if (Num == 5) {
+						System.out.println("시설예약");
+						
+					} else if (Num == 6) {
+						System.out.println("마일리지");
+						
+					} else if (Num == 7) {
+						System.out.println("진행중 이벤트");
+						
+					} else if (Num == 8) {
+						System.out.println("공지사항");
+						
+					} else if (Num == 9) {
+						System.out.println("로그아웃");
+						break;
+					} else {
+						pause();
+					}
+					
+
+					
+				} else if(userType == 2) {
+					// 직원에게 보여질 메뉴 출력
+				} else if(userType == 3) {
+					// 관리자이게 보여질 메뉴 출력
+				}
 			}
 		}
+		
+		
 			
 		
 	} //main
@@ -113,7 +150,11 @@ public class CulturalCenter {
 	public static void showUserMain() {
 		
 		System.out.println();
-		System.out.println("1. 회원정보 조회\t\t2.프로그램 신청");
+		System.out.println("1. 회원정보 조회\t2. 프로그램 신청");
+		System.out.println("3. 신청한 프로그램\t4. 진행중인 프로그램");
+		System.out.println("5. 시설예약\t\t6. 마일리지");
+		System.out.println("7. 진행중 이벤트\t8. 공지사항");
+		System.out.println("9. 로그아웃");
 		
 	}
 	
