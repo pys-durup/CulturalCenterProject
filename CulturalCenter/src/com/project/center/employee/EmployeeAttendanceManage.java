2fb7d4af847c756e9bffc8ee99ca82e050d577e9
cjdrkd7668 <74481989+cjdrkd7668@users.noreply.github.com>
2020-11-17 16:41:42 +0900

    직원 관리 기능 업데이트(6)
    직원 출근 기능과
    직원 퇴근 기능을 구현했습니다.
    추가로, 출근은 했으나 근무시간을 맞추지 못한 경우 조퇴로 기록되도록 하였습니다.

--- CulturalCenter/src/com/project/center/employee/EmployeeAttendanceManage.java
+++ CulturalCenter/src/com/project/center/employee/EmployeeAttendanceManage.java
@@ -17,17 +17,17 @@ public class EmployeeAttendanceManage {
 	
 	private String code = "";
 	private String name = "";
-	
-	public void checkEmployeeAccess() {
+
+	public boolean isEmployeeAccess() {
+		
+		employee.setCode("");
+		employee.setName("");
 		
 		Scanner scan = new Scanner(System.in);
 		
 		try {
 
 			BufferedReader reader = new BufferedReader(new FileReader(Path.EMPLOYEELIST));
-			
-			String line = null;
-			String[] list = new String[6];
 			
 			System.out.println("============================[근태 기록]===========================");
 			System.out.println();
@@ -40,59 +40,95 @@ public class EmployeeAttendanceManage {
 			String inputPassword = scan.nextLine();
 			System.out.println();
 			
+			String line = null;
+			String[] list = new String[6];
+			
 			while ((line = reader.readLine()) != null) {
 				list = line.split(",");
 				if (inputCode.equals(list[0]) && inputPassword.equals(list[3])) {
-					this.check = true;
+					check = true;
 					
 					employee.setCode(list[0]);
 					employee.setName(list[1]);
+					break;
+				} else {
+					check = false;
+					employee.setCode("");
+					employee.setName("");
 				}
 			}
 			
 			reader.close();
+			
+			if (check == false) {
+				mainToByFailure();
+				return false;
+			}
 			
 		} catch (Exception e) {
 			System.out.println("EmployeeAttendanceManage.checkEmployeeAccess()");
 			e.printStackTrace();
 		}
 		
+		return true;
 		
 	} //checkEmployeeAccess()
+
+	private void mainToByFailure() {
+		Scanner scan = new Scanner(System.in);
+		System.out.println("\t\t\t*** 경고 ***\t\t");
+		System.out.println("\t잘못된 정보를"
+						+ " 입력했습니다!");
+		System.out.println("\t  엔터를 눌러 메인화면으로"
+						+ " 이동해주세요.");
+		System.out.println("\t\t\t*** 경고 ***\t\t");
+		System.out.println();
+		scan.nextLine();
+	}
 	
 	public void viewEmployeeAttendance() {
-		
-		Scanner scan = new Scanner(System.in);
-		
-		
-		System.out.println("============================[근태 기록]===========================");
-		System.out.println();
-		System.out.printf("%s님, 반갑습니다. 원하는 메뉴 번호를 입력해주세요.", this.name);
-		System.out.println();
-		System.out.println("1. 출근 기록");
-		System.out.println();
-		System.out.println("2. 퇴근 기록");
-		System.out.println();
-		System.out.println("3. 메인 메뉴로 돌아가기");
-		System.out.println();
-		System.out.print("메뉴 입력 :");
-		String inputMenu = scan.nextLine();
-		System.out.println();
-		
-		switch(inputMenu) {
-		case "1":
-			setEmployeeAttendanceByClockIn();
-			break;
-		case "2":
-			updateEmployeeAttendance();
-			break;
-		case "3":
-			mainTo();
-			break;
-		default:
-			System.out.println("직원 관리를 종료합니다.");
-			break;
+
+		if (check) {
+			
+			Scanner scan = new Scanner(System.in);
+			
+			
+			System.out.println("============================[근태 기록]===========================");
+			System.out.println();
+			System.out.printf("%s님, 반갑습니다. 원하는 메뉴 번호를 입력해주세요.", employee.getName());
+			System.out.println();
+			System.out.println("1. 출근 기록");
+			System.out.println();
+			System.out.println("2. 퇴근 기록");
+			System.out.println();
+			System.out.println("3. 메인 메뉴로 돌아가기");
+			System.out.println();
+			System.out.print("메뉴 입력 :");
+			String inputMenu = scan.nextLine();
+			System.out.println();
+			
+			switch(inputMenu) {
+			case "1":
+				setEmployeeAttendanceByClockIn();
+				break;
+			case "2":
+				updateEmployeeAttendance();
+				break;
+			case "3":
+				mainTo();
+				break;
+			default:
+				System.out.println("근태 기록을 종료합니다.");
+				break;
+			}
+			
+		} else {
+			mainToByFailure();
 		}
+		
+	}
+
+	private void mainTo() {
 	}
 
 	private void setEmployeeAttendanceByClockIn() {
@@ -100,7 +136,7 @@ public class EmployeeAttendanceManage {
 		Calendar today = Calendar.getInstance();
 		String[] list = new String[6];
 		employee.setDate(String.format("%tF", today));
-		employee.setCommuteTime(String.format("%tT", today));
+		employee.setCommuteTime(String.format("%tk:%tM", today, today));
 		
 		try {
 
@@ -112,7 +148,7 @@ public class EmployeeAttendanceManage {
 			
 			while ((line = reader.readLine()) != null) {
 				list = line.split(",");
-				if (employee.getDate().equals(list[2]) && list[0].equals(this.code)) {
+				if (employee.getDate().equals(list[2]) && list[0].equals(this.employee.getCode())) {
 					
 					isRenewal = true;
 					
@@ -124,9 +160,7 @@ public class EmployeeAttendanceManage {
 			
 			reader.close();
 			
-			BufferedWriter writer = new BufferedWriter(new FileWriter(Path.EMPLOYEEATTENDANCE));
-			
-			dummy += String.format("%s, %s, %s, %s, %s ,%s"
+			dummy += String.format("%s,%s,%s,%s,%s,%s"
 									, employee.getCode()
 									, employee.getName()
 									, employee.getDate()
@@ -134,14 +168,29 @@ public class EmployeeAttendanceManage {
 									, employee.getWorkingTime()
 									, employee.getAttendance());
 			
-			
 			if (isRenewal) {
-				writer.write(dummy);
+				
+				try {
+
+					BufferedWriter writer = new BufferedWriter(new FileWriter(Path.EMPLOYEEATTENDANCE));
+
+					writer.write(dummy);
+					
+					writer.close();
+					
+				} catch (Exception e) {
+					System.out.println("EmployeeAttendanceManage.setEmployeeAttendanceByClockIn()");
+					e.printStackTrace();
+				}
+				
 				System.out.printf("%d월 %d일의 출근 시각이 갱신되었습니다."
 						, today.get(Calendar.MONTH) + 1
 						, today.get(Calendar.DATE)
 						);
+				System.out.println();
+				
 			} else {
+				
 				dummy = null;
 				employee.setEmployeeAttendanceIntoData();
 				
@@ -149,9 +198,9 @@ public class EmployeeAttendanceManage {
 						, today.get(Calendar.MONTH) + 1
 						, today.get(Calendar.DATE)
 						);
+				System.out.println();
 			}
 			
-			writer.close();
 			
 		} catch (Exception e) {
 			System.out.println("EmployeeAttendanceManage.setEmployeeAttendanceByClockIn()");
@@ -163,10 +212,137 @@ public class EmployeeAttendanceManage {
 
 	private void updateEmployeeAttendance() {
 		
+		Calendar clockOut = Calendar.getInstance();
+		Scanner scan = new Scanner(System.in);
+		boolean isClockOut = false;
+		boolean check = false;
+		
+		try {
+			String line = null;
+			String[] list = new String[6];
+					
+			BufferedReader reader = new BufferedReader(new FileReader(Path.EMPLOYEEATTENDANCE));
+					
+			String[] clockInMins = new String[2];
+			int clockOutMins = 60 * clockOut.get(Calendar.HOUR_OF_DAY)
+					+ clockOut.get(Calendar.MINUTE);
+			int workingMins = 0;
+			
+			while ((line = reader.readLine()) != null) {
+				
+				list = line.split(",");
+				
+				if (employee.getCode().equals(list[0]) 
+						&& String.format("%tF", clockOut).equals(list[2])
+						&& list[5].equals("결근")) {
+					
+					clockInMins = list[3].split(":");
+					workingMins = clockOutMins 
+							- (60 * Integer.parseInt(clockInMins[0])
+									+ Integer.parseInt(clockInMins[1]));
+					employee.setCommuteTime(list[3]);
+					employee.setDate(list[2]);
+					employee.setWorkingTime(String.format("%02d:%02d"
+							, workingMins / 60
+							, workingMins % 60)
+							);
+					employee.setAttendance(
+							workingMins < 480 ? "조퇴" : "출근"
+							);
+					
+					employee.setEmployeeAttendanceIntoData();
+					
+					System.out.printf("%s님, %s년 %s월 %s일 %s시에 퇴근 기록되었습니다.\n"
+							, employee.getName()
+							, clockOut.get(Calendar.YEAR)
+							, clockOut.get(Calendar.MONTH) + 1
+							, clockOut.get(Calendar.DATE)
+							, clockOut.get(Calendar.HOUR_OF_DAY)
+							);
+					
+					deleteFormal();
+					
+					check = true;
+					System.out.println("엔터를 누르시면, 메인 메뉴로 돌아갑니다.");
+					scan.nextLine();
+					break;
+					
+				} 
+				
+			}
+			
+			if(check == false) {
+				System.out.println("오늘 출근한 기록이 없습니다!");
+				System.out.println();
+				System.out.println("\t  엔터를 눌러 메인화면으로"
+						+ " 이동해주세요.");
+				System.out.println("\t\t\t*** 경고 ***\t\t");
+				System.out.println();
+				scan.nextLine();
+			}
+			
+			reader.close();
+					
+		} catch (Exception e) {
+			System.out.println("EmployeeAttendanceManage.updateEmployeeAttendance()");
+			e.printStackTrace();
+		}
+		
 	}
+	
+	private void deleteFormal() {
+		
+		Calendar c = Calendar.getInstance();
+		
+		try {
 
-	private void mainTo() {
+			BufferedReader reader = new BufferedReader(new FileReader(Path.EMPLOYEEATTENDANCE));
+			String dummy = "";
+			String line = null;
+			String[] list = new String[6];
+			
+			while ((line = reader.readLine()) != null) {
+				list = line.split(",");
+				if (list[4].equals("99:99") 
+						&& employee.getCode().equals(list[0]) 
+						&& employee.getDate().equals(String.format("%tF", c))) {
+				} else {
+					dummy += line + "\r\n";
+					
+				}
+				
+			}
+			
+			reader.close();
+			
+			BufferedWriter writer = new BufferedWriter(new FileWriter(Path.EMPLOYEEATTENDANCE));
+			
+			writer.write(dummy.substring(0, dummy.length() - 2));
+			
+			writer.close();
+
+		} catch (Exception e) {
+			System.out.println("EmployeeAttendanceManage.deleteFormal()");
+			e.printStackTrace();
+		}
 		
 	}
 
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
