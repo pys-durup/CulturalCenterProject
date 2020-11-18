b041399a2725fdadc089757799fa7d7f11748dc2
이은규 <dldmsrbek1@naver.com>
2020-11-18 18:44:29 +0900

    강의실 관리자
    삭제기능 빼고 구현완료 및
    메서드 나열
    deleteClassRoom() -강의실삭제
    modifyClassRoom() -강의실 수정
    findClassRoom() -강의실 조회
    selectNum() - 번호 입력
    selectNumClassRoom() - 강의실수정 번호입력
    deleteNumClassRoom() - 강의실 삭제 번호입력
    classRoomMain() - 강의실관리 메인
    back() - 뒤로가기
    programMain() -프로그렘관리 메인
    findProgramOnList() -진행중 프로그램조회
    findProgramOffList() -종료된 프로그램 조회
    modifyProgram() -프로그램 수정
    modifyNumprogram() -프로그램 수정 번호입력
    deleteProgram() -프로그램 삭제
    deleteNumProgram() -프로그램 삭제 번호입력
    insertProgram() - 프로그램등록

--- CulturalCenter/src/com/project/center/program/ClassRoomManage.java
+++ CulturalCenter/src/com/project/center/program/ClassRoomManage.java
@@ -1,21 +1,29 @@
 package com.project.center.program;
 
 import java.io.BufferedReader;
+import java.io.BufferedWriter;
 import java.io.File;
+import java.io.FileInputStream;
 import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
 import java.util.Scanner;
 
 import data.Path;
 
 public class ClassRoomManage {
 	
-	public static void main(String[] args) {
+	public static void main(String[] args) throws IOException {
 		
 		//강의실 관리 메인화면 출력
-		classRoomMain();
 		
+		classRoomMain();
 		while(true) {
 			int Num;
+			
 			// 원하는 번호입력
 			Num = selectNum();
 			
@@ -28,10 +36,16 @@ public class ClassRoomManage {
 			}else if(Num == 2) { //강의실 수정
 			
 				System.out.println("강의실 수정 선택");
+				System.out.println();
 				modifyClassRoom();
-				selectNumClassRoom();
-			
-			
+				modifyContent();
+				findClassRoom();
+				System.out.println("수정이 완료 되었습니다.");
+//				while(true) {
+//					classRoomMain();
+//					Num2 = selectNum();
+//				}
+
 			}else if(Num == 3) { //강의실 삭제
 		
 				System.out.println("강의실 삭제 선택");
@@ -51,27 +65,35 @@ public class ClassRoomManage {
 				
 			}
 			
-
-
-			
-			
-			
-		}
+	}
 			
 			
 			
 		
 	}//main
+
 	//뒤로 가기 메서드
 	private static void back() {
 		Scanner scan = new Scanner(System.in);
 		System.out.println();
 		System.out.print("뒤로 가시려면 B를 눌러주세요 : ");
 		String temp = scan.nextLine();
-		if(temp.equals("b") || temp.equals("B")) {
+		
+		while(true) {
 			
-			classRoomMain();
+			if(temp.equals("b") || temp.equals("B")) {
+				
+				classRoomMain();
+				break;
+			}else if (!temp.equals("b") || temp.equals("B")) {
+				System.out.print("뒤로 가시려면 B를 눌러주세요 : ");
+				temp = scan.nextLine();
+				
+				
+			}
 		}
+	
+	
 	}
 	
 	
@@ -103,19 +125,23 @@ public class ClassRoomManage {
 	
 	// 번호 입력 메서드
 	private static int selectNum() {
-		
+
 		// 사용자에게 번호를 입력받는다
+	
 		Scanner scan = new Scanner(System.in);
 		System.out.println();
 		System.out.print("번호를 선택하세요 : ");
-		return Integer.parseInt(scan.nextLine());
+		String temp = scan.nextLine();
+		return Integer.parseInt(temp);
+		//return Integer.parseInt(scan.nextLine());
 		
 	}
 	
 	//강의실 조회 메서드
 	public static void findClassRoom() {
 		
-		final String PATH = Path.PROGRAMLIST;
+		System.out.println("강의실번호\t프로그램코드\t프로그램명\t\t\t강사명\t시작날짜\t\t종료날짜\t\t정원");
+		final String PATH = Path.CLASSROOM;
 		File file = new File(PATH);
 		
 		try {
@@ -129,12 +155,15 @@ public class ClassRoomManage {
 				
 				String[] temp = line.split(",");
 				
-				System.out.printf("%s\t%s\t%-18s\t%s\t%s\n"
-						,temp[3]
-						,temp[2]
+				System.out.printf("%s\t%s\t%-18s\t%s\t%s\t%s\t%s\n"
+						,temp[0]
 						,temp[1]
+						,temp[2]
+						,temp[3]
 						,temp[4]
-						,temp[5]);
+						,temp[5]
+						,temp[6]);
+				
 			}
 			
 			reader.close();
@@ -146,10 +175,48 @@ public class ClassRoomManage {
 			
 		}
 	}
+
+	//강의실 수정 문구 메서드
+	private static void modifyContent() throws IOException {
+		
+		String fileName = "src\\data\\강의실.txt";
+		InputStream in = System.in;
+		BufferedReader reader = new BufferedReader(new InputStreamReader(in));
+		System.out.print("수정하고 싶은 강의실 정보 입력 : ");
+		String target = reader.readLine();
+		
+		System.out.print("수정 내용 입력: ");
+		  String change = reader.readLine();
+
+		  BufferedReader outinfo = new BufferedReader(new InputStreamReader(new FileInputStream(fileName)));
+		  String temp = null;
+		  ArrayList info = new ArrayList();
+		  
+		  while((temp=outinfo.readLine()) != null ) {
+		   info.add(temp.replaceAll(target, change));
+		  }
+		  
+		  BufferedWriter newInfo = new BufferedWriter(new FileWriter("src\\data\\강의실.txt", false)); 
+		  for(int i=0; i < info.size(); i++) {
+		   newInfo.write((String)info.get(i));
+		   newInfo.newLine();
+		  }
+		  
+		  newInfo.flush();
+		  newInfo.close();
+		  outinfo.close();
+		  reader.close();
+		  
+		  
+		
+
+	}
+	
 	
 	//강의실 수정 메서드
 	public static void modifyClassRoom() {
-		final String PATH = Path.PROGRAMLIST;
+		System.out.println("강의실번호\t프로그램코드\t프로그램명\t\t\t강사명\t시작날짜\t\t종료날짜\t\t정원");
+		final String PATH = Path.CLASSROOM;
 		File file = new File(PATH);
 		
 		try {
@@ -163,12 +230,15 @@ public class ClassRoomManage {
 				
 				String[] temp = line.split(",");
 				
-				System.out.printf("%s,%s,%s,%s,%s\n"
-						,temp[3]
-						,temp[2]
+				System.out.printf("%s\t%s\t%-18s\t%s\t%s\t%s\t%s\n"
+						,temp[0]
 						,temp[1]
+						,temp[2]
+						,temp[3]
 						,temp[4]
-						,temp[5]);
+						,temp[5]
+						,temp[6]);
+				
 			}
 			
 			reader.close();
@@ -180,12 +250,14 @@ public class ClassRoomManage {
 			
 		}
 		
+		
+			
 	}
 	
 	//강의실 삭제 메서드
 	public static void deleteClassRoom() {
-		
-		final String PATH = Path.PROGRAMLIST;
+		System.out.println("강의실번호\t프로그램코드\t프로그램명\t\t\t강사명\t시작날짜\t\t종료날짜\t\t정원");		
+		final String PATH = Path.CLASSROOM;
 		File file = new File(PATH);
 		
 		try {
@@ -198,13 +270,16 @@ public class ClassRoomManage {
 			while ((line = reader.readLine()) != null) {
 				
 				String[] temp = line.split(",");
-				
-				System.out.printf("%s,%s,%s,%s,%s\n"
-						,temp[3]
-						,temp[2]
+			
+				System.out.printf("%s\t%s\t%-18s\t%s\t%s\t%s\t%s\n"
+						,temp[0]
 						,temp[1]
+						,temp[2]
+						,temp[3]
 						,temp[4]
-						,temp[5]);
+						,temp[5]
+						,temp[6]);
+				
 			}
 			
 			reader.close();
