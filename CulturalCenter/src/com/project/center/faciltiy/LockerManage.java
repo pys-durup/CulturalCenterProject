122270a470f1c7d7da7259b1992f970183ef5b24
abcdana <74163296+abcdana@users.noreply.github.com>
2020-11-18 15:48:22 +0900

    사물함 현황 출력
    조장님 감사합니다

--- CulturalCenter/src/com/project/center/faciltiy/LockerManage.java
+++ CulturalCenter/src/com/project/center/faciltiy/LockerManage.java
@@ -27,10 +27,7 @@ public class LockerManage {
 		
 	public static void main(String[] args) throws IOException {
 		
-		//readLocker();
-		//printLocker();
-		rocker();
-		
+		rockerList();
 		
 	}//main(임시)
 	
@@ -38,21 +35,18 @@ public class LockerManage {
 	
 	
 	
-	
-	
-	
-	private static void rocker() {
+	private static void rockerList() {
 		
 		try {
 
-			BufferedReader lockerReader = new BufferedReader(new FileReader(Path.LOCKTEST));
-			BufferedReader userReader = new BufferedReader(new FileReader(Path.USERTEST));
+			BufferedReader lockerReader = new BufferedReader(new FileReader(Path.LOCKER));
+			BufferedReader userReader = new BufferedReader(new FileReader(Path.USERLIST));
+			
+			System.out.println("==============================================================================");
+			System.out.println("                                   사 물 함");
+			System.out.println("==============================================================================");
 			
 			String line = null;
-			String userCode = "";
-			String lockerNum = "";
-
-			int b = 0;
 
 			while ((line = lockerReader.readLine()) != null) {
 
@@ -62,7 +56,6 @@ public class LockerManage {
 						, ltemp[1] // 회원번호
 						, ltemp[2] // 시작일
 						, ltemp[3])); // 끝일
-				
 			}
 			
 			while ((line = userReader.readLine()) != null) {
@@ -75,37 +68,48 @@ public class LockerManage {
 			}
 			
 			
+			//Locker present state printing
 			Calendar c = Calendar.getInstance();
-			String present = String.format("%tF", c);
+			String present = String.format("%tF", c); //현재 날짜
 			
-			for (int i=0; i<l.size(); i++) {
-				
-				for (int j=0; j<u.size(); j++) {
-					
+			String line1 = "";
+			String line2 = "";
+
+			int b = 0;
+
+			for (int i = 0; i < l.size(); i++) {
+
+				for (int j = 0; j < u.size(); j++) {
+
 					if (l.get(i).getUserCode().equals(u.get(j).getCode())) {
-
-						System.out.printf("[%3s]", l.get(i).getLockerNum());
+						//l(사물함)의 사물함 번호 출력
+						line1 += String.format("[ %3s]\t", l.get(i).getLockerNum());
 						b++;
-
-						if (present.compareTo(l.get(i).getStartDate())>0 
-								&& present.compareTo(l.get(i).getEndDate())<0) {
-							
-							System.out.printf("%s\t", u.get(j).getName());
+						//현재날짜가 startDate(이용시작날짜)와 endDate(이용끝날짜) 사이에 존재한다.
+						//	-> 현재 본 회원이 사물함을 이용중이다. => 이용중인 사물함
+						if (present.compareTo(l.get(i).getStartDate()) > 0
+								&& present.compareTo(l.get(i).getEndDate()) < 0) {
+							line2 += String.format("%4s\t", u.get(j).getName());
+						// 	-> 이용중이지 않다면 빈 문자열 출력 => 이용 가능한 사물함	
 						} else {
-							System.out.print("  □\t");
+							line2 += String.format("   \t");
 						}
-	
-	
+
+						//line1, line2에 저장해 놓은 정보 출력(10개 단위)-> 초기화 -> 다시 반복
 						if (b % 10 == 0) {// 10으로 나누어 떨어지면 개행
+							System.out.println(line1);
 							System.out.println();
-							System.out.println();
+							System.out.println(line2);
+							System.out.println("\n");
+							line1 = "";		//문자열 초기화
+							line2 = "";		//문자열 초기화
 						}
 					}
 				}
 			}
 			
-			
-			
+			lockerReader.close();
+			userReader.close();
 
 		} catch (Exception e) {
 			System.out.println("LockerManage.rocker()");
@@ -115,147 +119,4 @@ public class LockerManage {
 	}//rocker() 연습
 
 
-
-
-
-
-
-
-	public static void readLocker() throws IOException {
-
-		System.out.println("============================================================================");
-		System.out.println("                                   사 물 함");
-		System.out.println("============================================================================");
-		System.out.println("[]\t[월]\t[화]\t[수]\t[목]\t[금]\t[토]\t[목]\t[금]\t[토]");
-
-		try {
-
-			BufferedReader lockerReader = new BufferedReader(new FileReader(Path.LOCKTEST));
-			BufferedReader userReader = new BufferedReader(new FileReader(Path.USERTEST));
-
-			String line = null;
-
-//			while ((line = reader.readLine()) != null) {
-//
-//				String[] temp = line.split(",");
-//
-//				l.add(new Locker(temp[0] // 락커번호
-//						, temp[1] // 회원번호
-//						, temp[2] // 시작일
-//						, temp[3])); // 끝
-
-				// 잘 읽어왔나 확인
-//				System.out.printf("%s\t%s\t%s\t%s\r\n"
-//									,temp[0]
-//									,temp[1]
-//									,temp[2]
-//									,temp[3]);
-
-//			int b = 0;
-//			
-//			for (int i = 1; i <= 100; i ++) {
-//				
-//				b++;
-//				
-//				if (b % 10 == 0) // 10으로 나누어 떨어지면 개행
-//				{
-//					System.out.println();
-//					System.out.println();
-//				}
-//			}
-			
-			
-			int b = 0;
-			
-				
-					while ((line = lockerReader.readLine()) != null) {
-
-						String[] temp = line.split(",");
-
-						l.add(new Locker(temp[0] // 락커번호
-								, temp[1] // 회원번호
-								, temp[2] // 시작일
-								, temp[3])); // 끝
-						
-						System.out.printf("(%3s)", temp[0]);
-						System.out.printf("%3s\t", temp[1]);
-						b++;
-
-						if (b % 10 == 0) // 10으로 나누어 떨어지면 개행
-						{
-							System.out.println();
-							System.out.println();
-						}
-					}
-					
-					
-					//System.out.println(l.get(0).getUserCode());
-//					
-//					//Arraylist 담은 객체 출력
-//					for (int i=0; i<l.size(); i++) {
-//						System.out.println(l.get(i).getUserCode());
-//					}
-			
-				
-
-//			}
-			
-			System.out.println();
-
-			//reader.close();
-			
-		} catch (Exception e) {
-			System.out.println("LockerManage.lockerPrint()");
-			e.printStackTrace();
-		}
-
-	}//락커 읽어오기
-	
-	
-	private static void printLocker() throws IOException {
-		
-		//1부터 100찍기 [사물함번호] 연습
-		int j = 0;
-		for (int i = 1; i <= 100; i ++) {
-
-			System.out.printf("%3d\t", i);
-			j++;
-			
-			if (j % 10 == 0) // 10으로 나누어 떨어지면 개행
-			{
-				System.out.println();
-				System.out.println();
-			}
-		}
-
-		
-		readLocker();
-		
-		
-		int b = 0;
-		for (int i = 1; i <= 100; i ++) {
-			
-			System.out.printf("%3d\t", i);
-			b++;
-			
-			if (b % 10 == 0) // 10으로 나누어 떨어지면 개행
-			{
-				System.out.println();
-				System.out.println();
-			}
-		}
-		
-		for (int i = 1; i <= 100; i ++) {
-			
-			//System.out.printf("%3d\t", );
-			b++;
-			
-			if (b % 10 == 0) // 10으로 나누어 떨어지면 개행
-			{
-				System.out.println();
-				System.out.println();
-			}
-		}
-		
-	}
 }
