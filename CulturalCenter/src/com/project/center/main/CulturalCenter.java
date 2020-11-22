57d4113de76fdf9193662fe73a80c7650c9f45c1
durup <enenfnq@gmail.com>
2020-11-22 16:33:02 +0900

    로그인 - 로그인 실패시 프로그램 종료현상 수정

--- CulturalCenter/src/com/project/center/main/CulturalCenter.java
+++ CulturalCenter/src/com/project/center/main/CulturalCenter.java
@@ -53,8 +53,10 @@ public class CulturalCenter {
 			Num = selectNum(); 
 			
 			if(Num == 1) { // 로그인
-				System.out.println("로그인 선택");
+//				System.out.println("로그인 선택");
 				login = UserLogin.checkUser();
+				
+
 				// 로그인처리 메서드
 				// public void isLogin()? 로그인처리?
 				// public void createAccount(){} 로그인 성공하면?
@@ -62,13 +64,13 @@ public class CulturalCenter {
 				
 				
 			} else if(Num == 2) { // 회원가입
-				System.out.println("회원가입 선택");
+//				System.out.println("회원가입 선택");
 				UserRegister.infoRegister();
 				// 회원가입처리 메서드
 				// 회원 가입 완료후 다시 메인화면으로
 			} else if(Num == 3) {
 				// 아이디/비밀번호 찾기 메서드
-				System.out.println("아이디/비밀번호 찾기 선택");
+//				System.out.println("아이디/비밀번호 찾기 선택");
 				UserFind.findUser();
 				
 				
@@ -81,127 +83,130 @@ public class CulturalCenter {
 				pause();
 			}
 			
-			System.out.println("프로그램 진행 . . . .");
-			System.out.println("사용자 : " + login.getName());
-			System.out.println("아이디 : " + login.getId());
-			
-			
-			while (true) {
+			if(login != null) {
+				System.out.println("프로그램 진행 . . . .");
+				System.out.println("사용자 : " + login.getName());
+				System.out.println("아이디 : " + login.getId());				
 				
-				
-				// 1. 회원일때 2. 직원일때 3. 관리자 일때
-				if(login.getType() == 1) {
-					// 회원에게 보여질 메뉴 출력
-					showUserMain();
-					Num = selectNum();
+				while (true) {
 					
-					if(Num == 1) {
+					
+					// 1. 회원일때 2. 직원일때 3. 관리자 일때
+					if(login.getType() == 1) {
+						// 회원에게 보여질 메뉴 출력
+						showUserMain();
+						Num = selectNum();
+						
+						if(Num == 1) {
 //						System.out.println("회원정보 조회");
-						UserMyPage.showMyPage(login);
-						
-					} else if (Num == 2) {
+							UserMyPage.showMyPage(login);
+							
+						} else if (Num == 2) {
 //						System.out.println("프로그램 신청");
-						ProgramManage pm = new ProgramManage();
-						pm.createApplyProgram(login);
-						
-					} else if (Num == 3) {
+							ProgramManage pm = new ProgramManage();
+							pm.createApplyProgram(login);
+							
+						} else if (Num == 3) {
 //						System.out.println("프로그램 등록현황");
-						ProgramRegistrationList pr = new ProgramRegistrationList(login);
-						pr.createProgramRegistorList();
-
-					} else if (Num == 4) {
-						GymReservation.reservationGym(login);
+							ProgramRegistrationList pr = new ProgramRegistrationList(login);
+							pr.createProgramRegistorList();
+							
+						} else if (Num == 4) {
+							GymReservation.reservationGym(login);
 //						System.out.println("시설예약");
-						
-					} else if (Num == 5) {
+							
+						} else if (Num == 5) {
 //						System.out.println("시설예약 확인");
-						FacilityReservation.findReservationList(login);
-
-					} else if (Num == 6) { // 마일리지
-						MileageConfirm mc = new MileageConfirm();
-						mc.showMyMileage(login);
-					} else if (Num == 7) { 
+							FacilityReservation.findReservationList(login);
+							
+						} else if (Num == 6) { // 마일리지
+							MileageConfirm mc = new MileageConfirm();
+							mc.showMyMileage(login);
+						} else if (Num == 7) { 
 //						System.out.println("진행중 이벤트");
-						MileageConfirm mc = new MileageConfirm();
-						mc.showEvent();
-					} else if (Num == 8) {
+							MileageConfirm mc = new MileageConfirm();
+							mc.showEvent();
+						} else if (Num == 8) {
 //						System.out.println("공지사항");
-						NoticeManage notice = new NoticeManage();
-						//notice.getNotice();
-						notice.userNotice();
-						//notice.findNoticeList();
-					} else if (Num == 9) {
-						System.out.println("로그아웃");
+							NoticeManage notice = new NoticeManage();
+							//notice.getNotice();
+							notice.userNotice();
+							//notice.findNoticeList();
+						} else if (Num == 9) {
+							System.out.println("로그아웃");
+							break;
+						} else {
+							pause();
+						}
+						
+					} else if(login.getType() == 2) {
+						
+						// 직원에게 보여질 메뉴 출력
+						employee.viewEmployeeAttendance(login);
 						break;
-					} else {
-						pause();
-					}
-					
-				} else if(login.getType() == 2) {
-
-					// 직원에게 보여질 메뉴 출력
-					employee.viewEmployeeAttendance(login);
-					break;
-				} else if(login.getType() == 3) {
-					// 관리자에게 보여질 메뉴 출력
-					//		System.out.println("1. 직원 등록\t2. 직원 수정");
-					//System.out.println("3. 직원 조회\t4. 직원 근태 조회");
-					showManageMain();
-					Num = selectNum();
-					if(Num == 1) {
-						administer.checkEmployeeManage();
-						administer.viewEmployeeManage();
-						
-					} else if (Num == 2) {
-						administer.findEmployeeAttendanceList();
-						
-					} else if (Num == 3) {
-						//System.out.println("회원 관리");
-						UserManage um = new UserManage();
-						um.userManageMain();
-						
-					} else if (Num == 4) {
-						//System.out.println("사물함 관리");
-						LockerManage lm = new LockerManage();
-						lm.lockerManageMain();
-						
-					} else if (Num == 5) {
-						System.out.println("강의실 관리");
-						ClassRoomManage crm = new ClassRoomManage();
-						makeClassRoom();
-						crm.classRoomManageMain();
-					} else if (Num == 6) {
-						System.out.println("프로그램 관리");
-						ProgramManageBeta pmb = new ProgramManageBeta();
-						pmb.programManageBetaMain();
-					} else if (Num == 7) {
-						//System.out.println("진행중 이벤트");
-						Event event = new Event();
-						event.main(null);
-						
-					} else if (Num == 8) {
-						//System.out.println("공지사항");
-						NoticeManage notice = new NoticeManage();
-						notice.main(null);
-						
-					} else if (Num == 9) {
-						//System.out.println("시설예약");
-						SeminarManage seminar = new SeminarManage();
-						seminar.main(null);
-						
-					} else if (Num == 10) {
-						System.out.println("출결조회");
-						ProgramAttendanceManage padm = new ProgramAttendanceManage();
-						padm.createAttendanceMenu();
-					} else if (Num == 0) {
-						System.out.println("로그아웃");
-						break;
-					} else {
-						pause();
+					} else if(login.getType() == 3) {
+						// 관리자에게 보여질 메뉴 출력
+						//		System.out.println("1. 직원 등록\t2. 직원 수정");
+						//System.out.println("3. 직원 조회\t4. 직원 근태 조회");
+						showManageMain();
+						Num = selectNum();
+						if(Num == 1) {
+							administer.checkEmployeeManage();
+							administer.viewEmployeeManage();
+							
+						} else if (Num == 2) {
+							administer.findEmployeeAttendanceList();
+							
+						} else if (Num == 3) {
+							//System.out.println("회원 관리");
+							UserManage um = new UserManage();
+							um.userManageMain();
+							
+						} else if (Num == 4) {
+							//System.out.println("사물함 관리");
+							LockerManage lm = new LockerManage();
+							lm.lockerManageMain();
+							
+						} else if (Num == 5) {
+							System.out.println("강의실 관리");
+							ClassRoomManage crm = new ClassRoomManage();
+							makeClassRoom();
+							crm.classRoomManageMain();
+						} else if (Num == 6) {
+							System.out.println("프로그램 관리");
+							ProgramManageBeta pmb = new ProgramManageBeta();
+							pmb.programManageBetaMain();
+						} else if (Num == 7) {
+							//System.out.println("진행중 이벤트");
+							Event event = new Event();
+							event.main(null);
+							
+						} else if (Num == 8) {
+							//System.out.println("공지사항");
+							NoticeManage notice = new NoticeManage();
+							notice.main(null);
+							
+						} else if (Num == 9) {
+							//System.out.println("시설예약");
+							SeminarManage seminar = new SeminarManage();
+							seminar.main(null);
+							
+						} else if (Num == 10) {
+							System.out.println("출결조회");
+							ProgramAttendanceManage padm = new ProgramAttendanceManage();
+							padm.createAttendanceMenu();
+						} else if (Num == 0) {
+							System.out.println("로그아웃");
+							break;
+						} else {
+							pause();
+						}
 					}
 				}
 			}
-		}
+			
+			
+		} // first while
 		
 		
 			
@@ -230,7 +235,7 @@ public class CulturalCenter {
 		Scanner scan = new Scanner(System.in);
 		System.out.println("엔터키를 누르면 이전화면으로 돌아갑니다.");
 		scan.nextLine();
-		for(int i=0 ; i<10 ; i++) {
+		for(int i=0 ; i<50 ; i++) {
 			System.out.println();
 		}
 		
